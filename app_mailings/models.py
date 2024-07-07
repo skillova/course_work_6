52b34fff54ea9f778a35f4623079ddc1828ee94c
Skillova <skillova@yandex.ru>
2024-07-07 11:58:29 +0000

    create model mailings

---
+++ app_mailings/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+
+from app_customers.models import Customers
+
+STATUS_CHOICES = [
+    ('created', 'Создана'),
+    ('active', 'Запущена'),
+    ('finished', 'Завершена'),
+]
+
+INTERVAL_CHOICES = [
+    ('once', 'разовая'),
+    ('daily', 'ежедневно'),
+    ('weekly', 'раз в неделю'),
+    ('monthly', 'раз в месяц'),
+]
+
+ACTIVE_CHOICES = [
+    (True, 'Активна'),
+    (False, 'Неактивна'),
+]
+
+
+class Message(models.Model):
+    #  Django модель письма (eng: Message).
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Тема',
+    )
+    content = models.TextField(
+        verbose_name='Содержание',
+    )
+
+    class Meta:
+        #  Настройка для наименования одного и набора объектов
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+    def __str__(self):
+        #  Строковое отображение объекта
+        return self.title
+
+
+class MailSettings(models.Model):
+    #  Django модель настройки рассылки (eng: Message).
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        verbose_name='Сообщение',
+    )
+    sending_to = models.ManyToManyField(
+        Customers,
+        verbose_name='Адресат',
+    )
+    description = models.CharField(
+        max_length=150,
+        verbose_name='Описание',
+    )
+    start_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Начало рассылки',
+    )
+    end_date = models.DateTimeField(
+        verbose_name='Конец рассылки',
+    )
+    interval = models.CharField(
+        default='once',
+        max_length=10,
+        choices=INTERVAL_CHOICES,
+        verbose_name='Периодичность',
+    )
+    status = models.CharField(
+        default='created',
+        max_length=10,
+        choices=STATUS_CHOICES,
+        verbose_name='Статус',
+    )
+    is_active = models.BooleanField(
+        default=True,
+        choices=ACTIVE_CHOICES,
+        verbose_name='Активность',
+    )
+
+    class Meta:
+        #  Настройка для наименования одного и набора объектов
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+    def __str__(self):
+        #  Строковое отображение объекта
+        return self.description
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'app_customers.apps.AppCustomersConfig',
+    'app_mailings.apps.AppMailingsConfig',
 ]
 
 MIDDLEWARE = [
