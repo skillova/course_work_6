3e9cbc90af49d1959be15866ea201cf05a2638f3
Skillova <skillova@yandex.ru>
2024-07-09 22:25:36 +0000

    авторизация пользователя по имейлу

--- app_users/urls.py
+++ app_users/urls.py
@@ -2,12 +2,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from app_users.apps import AppUsersConfig
-from app_users.views import UserCreateView
+from app_users.views import UserCreateView, email_verification
 
 app_name = AppUsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='app_users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', UserCreateView.as_view(), name='register')
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
 ]
